b5e7f722f09bcd087bda431d6311183819ac409a
William Chenyin <welcomehomemutms7@gmail.com>
2026-05-05 19:43:04 -0400

    m

--- Hairrison2/vite.config.ts
+++ Hairrison2/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   define: {
     // Analytics: Mark this project as created via create-cloudinary-react CLI
     'process.env.CLOUDINARY_SOURCE': '"cli"',
